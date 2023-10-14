30bba571a4057039d81049be639cb7d6916bf512
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-14 16:05:11 -0400

    smart contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,2 +1,11 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNft {
+    constructor() {}
+}
