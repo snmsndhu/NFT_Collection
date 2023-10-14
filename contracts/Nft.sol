c92de5c5652e42919c59a8158f9faa804c74fdd4
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-14 16:15:24 -0400

    smart contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -6,6 +6,19 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNft {
-    constructor() {}
+contract MyNft is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    constructor() ERC721("MyNFT", "NFT"){}
+
+    function mintNFT(address recipent, string memory tokenURI)
+    public onlyOwner
+    returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipent, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
 }
