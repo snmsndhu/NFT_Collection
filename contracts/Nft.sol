88c8652c4743b042378e928467b236d98c56e4de
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-14 21:18:15 -0400

    Smart-Contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract MyNft is ERC721URIStorage, Ownable {
    uint256 private _tokenIdCounter;
     constructor() ERC721("MyNFT", "NFT"){}
@@ -12,7 +11,7 @@ contract MyNft is ERC721URIStorage, Ownable {
     function mintNFT(address recipent, string memory tokenURI)
     public onlyOwner
     returns (uint256) {
-       uint256 newItemId = _tokenIdCounter;
+       uint256 newItemId = _tokenIdCounter++;
         _mint(recipent, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
