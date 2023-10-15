e5c3ab7fe201fc9403eff0f3585c5544b11e6bf3
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-14 21:11:10 -0400

    Smart-Contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -2,21 +2,17 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract MyNft is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+   uint256 private _tokenIdCounter;
     constructor() ERC721("MyNFT", "NFT"){}
 
     function mintNFT(address recipent, string memory tokenURI)
     public onlyOwner
     returns (uint256) {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
+       uint256 newItemId = _tokenIdCounter;
         _mint(recipent, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
