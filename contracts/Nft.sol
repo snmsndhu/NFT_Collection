4bc23891be43d877a1f052f1a6f88603832afbd3
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-16 12:03:57 -0400

    Smart-Contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -15,4 +15,15 @@ contract MyNft is ERC721URIStorage {
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
     }
+
+    function generateCharacter(uint256 _tokenIdCounter) public returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">'
+            '<rect width="100%" height="100%" fill="black" />'
+            '<text x="50%" y="50%" fill="white" font-size="48" text-anchor="middle">Your Name</text>'
+            '</svg>'
+            );
+            return string(abi.encodePacked(
+            ));
+    }
 }
