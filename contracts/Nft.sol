69a2897fb8cea5ff27c6afeb75b47f247c1aac8e
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-17 11:44:52 -0400

    Smart-Contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-contract MyNft is ERC721URIStorage {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+contract MyNft is ERC721Enumerable, Ownable {
    uint256 private _tokenIdCounter;
     constructor() ERC721("MyNFT", "NFT"){}
 
