70f5944e8432371073cadac848de6ab921831c36
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-17 11:48:16 -0400

    Smart-Contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -5,7 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 contract MyNft is ERC721Enumerable, Ownable {
-   uint256 private _tokenIdCounter;
+   using Strings for uint256;
+
+   string _baseTokenURI;
+
+   uint256 public _price = 0.01 ether;
+
+   bool public _paused;
+
+   uint256 public maxTokenIds = 10;
+
+   uint256 public tokenIds;
+
+   modifier onlyWhenNotPaused {
+    require(!_paused, "contract currently paused");
+    _;
+   }
     constructor() ERC721("MyNFT", "NFT"){}
 
     function mintNFT(address recipent, string memory tokenURI)
