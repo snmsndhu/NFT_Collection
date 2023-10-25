c2c6bb02cc55776316c5398f07e72eda8ed6cdd5
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-25 10:35:22 -0400

    Front end

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,3 +3,13 @@ import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
+
+export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
+
+  const web3ModalRef = useRef();
+}
