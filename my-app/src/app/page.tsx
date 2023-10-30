1b1690c446b856d0da54ee701b596cb5e82e7a1e
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-30 12:49:36 -0400

    Front end

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,41 +5,7 @@ import Web3Modal from "web3modal";
 import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
 
 export default function Home() {
-  const [walletConnected, setWalletConnected] = useState(false);
-
-  const [loading, setLoading] = useState(false);
-
-  const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
-
-  const web3ModalRef = useRef();
-
-  const publicMint = async () => {
-    try {
-      console.log("Public Mint");
-
-      const signer = await getProviderOrSigner(true);
-
-      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
-
-      const tx = await nftContract.mint({
-        value: utils.parseEther("0.01"),
-      });
-      setLoading(true);
-
-      await tx.wait();
-      setLoading(false);
-      window.alert("you successfully mintes a NFT");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const connectWallet = async () => {
-    try {
-      await getProviderOrSigner();
-      setWalletConnected(true);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  <div>
+    <p className="text-white">hello world</p>
+  </div>;
 }
